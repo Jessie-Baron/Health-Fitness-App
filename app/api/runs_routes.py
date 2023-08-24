4330ff99e70a4683e367efebb7139d099c3e2782
Tristram Reed <tar123@txstate.edu>
2023-08-24 15:06:59 -0500

    runs api with test data

--- app/api/runs_routes.py
+++ app/api/runs_routes.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from app.models import User, db
 from app.models.runs import Runs
 import datetime
+import json
 
 runs_routes = Blueprint('runs', __name__)
 
@@ -23,18 +24,25 @@ def get_user_runs():
 
 @runs_routes.route('/<int:id>')
 def get_user_run_by_id(id):
-    verified_user = verify_user()
-    temp_list = verified_user['runs']
+    #verified_user = verify_user()
+    cur_user ={
+        'email': 'test@email.com'
+    }
+    temp = json.loads(User.objects(email=cur_user['email']).to_json())
+    temp_list = temp[0]['runs']
     for run in temp_list:
-        if (run['id'] == id):
+        if (run['run_id'] == id):
             return run
     return "error"
 
 @runs_routes.route('/', methods=['POST'])
 def create_run():
-    verified_user = verify_user()
+    #verified_user = verify_user()
+    cur_user ={
+        'email': 'test@email.com'
+    }
     req_body = request.json
-    new_run = Runs(date=datetime.datetime.now(), duration=req_body["duration"], distance=req_body["distance"])
-    update_run = User.objects(email=verified_user['email'])
-    update_run.update(add_to_set__runs=[new_run])
+    new_run = Runs(date=datetime.datetime.now().replace(microsecond=0), duration=req_body["duration"], distance=req_body["distance"])
+    add_run = User.objects(email=cur_user['email'])
+    add_run.update(add_to_set__runs=[new_run])
     return "success"
