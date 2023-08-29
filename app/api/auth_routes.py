a955b8c12e100df05a9f1a6fa21b8378223e981d
Tristram Reed <tar123@txstate.edu>
2023-08-29 14:08:51 -0500

    remove run/all runs endpoints

--- app/api/auth_routes.py
+++ app/api/auth_routes.py
@@ -3,6 +3,7 @@ from app.models import User, db
 from app.forms import LoginForm
 from app.forms import SignUpForm
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash
 import json
 
 auth_routes = Blueprint('auth', __name__)
@@ -72,7 +73,7 @@ def sign_up():
         user = User(
             username=form.data['username'],
             email=form.data['email'],
-            hashed_password=form.data['password'],
+            hashed_password=generate_password_hash(form.data['password']),
             runs=list()
         )
         user.save()
--- app/api/runs_routes.py
+++ app/api/runs_routes.py
@@ -10,17 +10,12 @@ runs_routes = Blueprint('runs', __name__)
 @runs_routes.route('/', methods=['GET'])
 @login_required
 def get_user_runs():
-    #verified_user = verify_user()
-    # user_runs = {
-    #     'runs': verified_user['runs']
-    # }
     user_list = User.objects(email=current_user.email)
     return jsonify(user_list[0]["runs"]) #user_runs
 
 @runs_routes.route('/<int:id>')
 @login_required
 def get_user_run_by_id(id):
-    #verified_user = verify_user()
     
     temp = json.loads(User.objects(email=current_user.email).to_json())
     temp_list = temp[0]['runs']
@@ -44,9 +39,6 @@ def create_run():
 def update_run(id):
     temp = json.loads(User.objects(email=current_user.email).to_json())
     req_body = request.json
-    # temp.runs.filter(run_id=id).update(distance=req_body['distance'], duration=req_body['duaration'])
-    # temp.save()
-    # return "success"
     temp_list = temp[0]['runs']
     for run in temp_list:
         if (run['run_id'] == id):
@@ -60,4 +52,24 @@ def update_run(id):
         
     return "error"
 
-    
+@runs_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_run(id):
+    temp = json.loads(User.objects(email=current_user.email).to_json())
+    temp_list = temp[0]['runs']
+    for run in temp_list:
+        if (run['run_id'] == id):
+            temp_list.remove(run)
+            
+            update_user = User.objects(email=current_user.email)
+            update_user.update(set__runs=temp_list)
+            return "success"
+        
+    return "error"
+
+@runs_routes.route('/', methods=['DELETE'])
+@login_required
+def delete_all_run(id):
+    update_user = User.objects(email=current_user.email)
+    update_user.update(set__runs=list())
+    return "All runs deleted"
